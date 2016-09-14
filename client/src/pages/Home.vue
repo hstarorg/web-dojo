8b19e36d68f35a5842755c135a8a8627b711ee4a
Jay.M.Hu <jay.m.hu@newegg.com>
2016-09-14 17:44:18 +0800

    Go

---
+++ client/src/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="page">
+    <p>
+      To get started, edit files in <code>./client</code> and save.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Home from './../pages/Home';
 
 Vue.use(Router);
 
@@ -8,9 +9,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: {
-        template: 'Home'
-      }
+      component: Home
     }
   ]
 })
