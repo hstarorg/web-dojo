35d0dfed90e9b09d214e387bdce69bfbfd544d09
Jay.M.Hu <hm910705@163.com>
2017-01-20 23:23:21 +0800

    db config change

--- server/src/config.js
+++ server/src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   port: 7411,
   apiPrefix: '/api/v1',
-  mongoAddress: 'mongodb://localhost/test'
+  // mongoAddress: 'mongodb://xxx:xxx@218.241.17.172:27017?authSource=admin&ext.auth.manuallyDbs=dojo'
+  mongoAddress: 'mongodb://localhost/dojo'
 };
