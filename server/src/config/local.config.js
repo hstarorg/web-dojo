81f97d6e469bca90e89997039157113c5cfd1cd1
Hstarorg <hm910705@163.com>
2018-04-21 00:20:31 +0800

    Adjust config folder

--- server/src/config/local.config.js
+++ server/src/config/local.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  port: 7411,
-  apiPrefix: '/api/v1',
   mongoAddress: 'mongodb://192.168.1.200/dojo',
   ssoApp: {
     authAddress: 'https://sso.hstar.org/api/v1/get_user_info',
     appKey: '4f0b9057-c02a-401b-b73c-c6a5dec99073',
-    appSecret: 'xxx'
+    appSecret: '5f5b044190e9497f98fe9ddb5f2c1d3505a1432b97bc437a99aa09f3ca768fe6'
   }
 };
